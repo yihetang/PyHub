355498f41d6753593e235aeb69e856b609e1c170
yihetang <2116575090@qq.com>
2018-11-17 22:28:57 +0800

    dog

---
+++ Python_Class/your_elec_car.py
@@ -0,0 +1,7 @@
+"""Your elec car"""
+
+from electric_car import ElectricCar
+
+your_elec_car = ElectricCar('fox', 'model-1', 2016)
+your_elec_car.battery.describe_battery()
+your_elec_car.battery.get_range()
