d4b03ce8cad505f5d646bc5d7ab1eab02c549287
yihetang <2116575090@qq.com>
2018-11-17 15:36:00 +0800

    dog

--- Python_Class/Dog.py
+++ Python_Class/Dog.py
@@ -3,14 +3,22 @@
 
 class Dog:
     """Just mimick the behavior of a small dog"""
-    def __init(self, name, age):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def sit(self):
         """mimick the action of a small dog """
-        print(self.name.title() + "is now sitting.")
+        print(self.name.title() + " is now sitting.")
 
     def roll_over(self):
         """mimick the rollover of a small dog"""
-        print(self.name.title() + "rolled over before you!")
+        print(self.name.title() + " rolled over before you!")
+
+
+# 実例化
+my_dog = Dog('kawayi kitty', 10)
+my_dog.sit()
+my_dog.roll_over()
+
+
